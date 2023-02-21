484ec835832db7b3eb62db84f78d405a014a0267
Mostafa Hosseini <kavrindev@gmail.com>
2023-02-21 23:18:26 +0330

    Finish Day01

---
+++ Day001/HacKingWithSwiftUiDay001.playground/Contents.swift
@@ -0,0 +1,93 @@
+import Cocoa
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+let actor = "Denzel Washington"
+
+let fileName = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(fileName.hasSuffix(".jpg"))
+
+let score = 10
+
+let reallyBig = 100000000
+let reallyBig2 = 100000000
+let reallyBig3 = 100000000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+
+counter += 5
+print(counter)
+
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+print(120.isMultiple(of: 3))
+
+let numberDouble = 0.1 + 0.2
+print(numberDouble)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var nameS = "Nicolas Cage"
+nameS = "John Travolta"
+
+// var nameD = "Nicolas Cage"
+// nameD = 57
+
+var rating = 5.0
+rating *= 2
