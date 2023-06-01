ca4fd2c886b0201e811163ddb622fc90c35f0ade
Mostafa Hosseini <kavrindev@gmail.com>
2023-06-01 15:47:19 +0330

    Finish Day 50

---
+++ Project10-CupcakeCorner/CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,58 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Mostafa Hosseini on 6/1/23.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    @Published var specialRequestEnable = false {
+        didSet {
+            if !specialRequestEnable {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+    
+    @Published var name = ""
+    @Published var streetAddress = ""
+    @Published var city = ""
+    @Published var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Double {
+        // $2 per cake
+        var cost = Double(quantity) * 2
+        
+        // complicated cakes cost more
+        cost += (Double(type) / 2)
+        
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+        
+        // $1/cake for extra frosting
+        if addSprinkles {
+            cost += Double(quantity) / 2
+        }
+        
+        return cost
+    }
+     
+}
