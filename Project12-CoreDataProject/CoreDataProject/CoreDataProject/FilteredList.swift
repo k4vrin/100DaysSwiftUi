c6494d5f16a428db4cd03d893a373093400eb017
Mostafa Hosseini <kavrindev@gmail.com>
2023-06-25 15:22:40 +0330

    Finish Day 59

--- Project12-CoreDataProject/CoreDataProject/CoreDataProject/FilteredList.swift
+++ Project12-CoreDataProject/CoreDataProject/CoreDataProject/FilteredList.swift
@@ -9,13 +9,12 @@ import SwiftUI
 import CoreData
 
 struct FilteredList<T: NSManagedObject, Content: View>: View {
-    
     @FetchRequest var fetchRequest: FetchedResults<T>
     @ViewBuilder let content: (T) -> Content
     
     
-    init(filterKey: String, filterValue: String, content: @escaping (T) -> Content) {
-        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+    init(predicate: Predicate = .beginsWith(caseSensetive: true), sortDesc: [NSSortDescriptor] , filterKey: String, filterValue: String, content: @escaping (T) -> Content) {
+        _fetchRequest = FetchRequest<T>(sortDescriptors: sortDesc, predicate: NSPredicate(format: "%K \(predicate.description) %@", filterKey, filterValue))
         self.content = content
     }
     
@@ -25,4 +24,39 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
             self.content(item)
         }
     }
+}
+
+
+enum Predicate: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .beginsWith(let caseSensetive):
+            if caseSensetive {
+                return "BEGINSWITH"
+            } else {
+                return "BEGINSWITH[c]"
+            }
+        case .endsWith(let caseSensetive):
+            if caseSensetive {
+                return "ENDSWITH"
+            } else {
+                return "ENDSWITH[c]"
+            }
+        case .inside: return "IN"
+        case .lesser: return "<"
+        case .greater: return ">"
+        case .contain(let caseSensetive):
+            if caseSensetive {
+                return "CONTAINS"
+            } else {
+                return "CONTAINS[c]"
+            }
+        }
+    }
+    case beginsWith(caseSensetive: Bool)
+    case endsWith(caseSensetive: Bool)
+    case inside
+    case lesser
+    case greater
+    case contain(caseSensetive: Bool)
 }
---
+++ Project12-CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Mostafa Hosseini on 6/25/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
