7c437036ca406e63dddd8652226d850dbc6a960a
Mostafa Hosseini <kavrindev@gmail.com>
2023-08-13 19:47:40 +0330

    Finish Day 88

--- Project17-Flashzilla/Flashzilla/Flashzilla/ContentView.swift
+++ Project17-Flashzilla/Flashzilla/Flashzilla/ContentView.swift
@@ -8,16 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    
     var body: some View {
-        VStack {
-            AccessibilityNeeds()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    ForEach(0 ..< cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
     }
 }
