129799ae356652bf8ae79d93ecfb5d0bc349bc21
Mostafa Hosseini <kavrindev@gmail.com>
2023-08-14 13:15:06 +0330

    Finish Day 89

--- Project17-Flashzilla/Flashzilla/Flashzilla/CardView.swift
+++ Project17-Flashzilla/Flashzilla/Flashzilla/CardView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct CardView: View {
-    
     let card: Card
     var removal: (() -> Void)? = nil
     
+    @Environment(\.accessibilityDifferentiateWithoutColor) var dwoColor
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.white)
+                .fill(
+                    dwoColor
+                        ? .white
+                        : .white.opacity(1 - Double(abs(offset.width / 50)))
+                )
+                
+                .background(
+                    dwoColor
+                        ? nil
+                        : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             
             VStack {
@@ -34,7 +45,6 @@ struct CardView: View {
             }
             .padding()
             .multilineTextAlignment(.center)
-            
         }
         .frame(width: 450, height: 250)
         .rotationEffect(.degrees(Double(offset.width / 5)))
@@ -56,7 +66,6 @@ struct CardView: View {
         .onTapGesture {
             isShowingAnswer.toggle()
         }
-        
     }
 }
 
