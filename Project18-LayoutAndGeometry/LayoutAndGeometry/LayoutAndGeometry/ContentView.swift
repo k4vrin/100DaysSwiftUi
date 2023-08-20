71fee66848b9ef2fa645aa14d9e9c689306eba34
Mostafa Hosseini <kavrindev@gmail.com>
2023-08-20 15:44:17 +0330

    Finish Day 94

--- Project18-LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ Project18-LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0 ..< 50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                Color(
+                                    hue: min(geo.frame(in: .global).minY / fullView.size.height, 1),
+                                    saturation: 1,
+                                    brightness: 1
+                                )
+                            )
+                            .rotation3DEffect(
+                                .degrees(
+                                    geo.frame(in: .global).minY - fullView.size.height / 2
+                                ) / 5,
+                                axis: (x: 0, y: 1, z: 0)
+                            )
+                            .opacity(
+                                geo.frame(in: .global).minY / 300
+                            )
+                            .scaleEffect(
+                                x: scaleSize(geo, fullView),
+                                y: scaleSize(geo, fullView)
+                            )
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .padding()
+    }
+    
+    func scaleSize(_ geo: GeometryProxy, _ fullView: GeometryProxy) -> CGFloat {
+        if geo.frame(in: .global).minY <= (fullView.size.height / 2) {
+            return max((1 + (geo.frame(in: .global).minY / (fullView.size.height/2))) * 0.5, 0.5)
+        } else {
+            return max((1 + (fullView.size.height/2) / (geo.frame(in: .global).minY)) * 0.5, 0.5)
+        }
     }
 }
 
