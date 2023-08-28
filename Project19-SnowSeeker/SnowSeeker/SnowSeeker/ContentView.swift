c299bff86454750a53f6fcfca4576bf92ef02095
Mostafa Hosseini <kavrindev@gmail.com>
2023-08-28 16:24:35 +0330

    Finish Day 99

--- Project19-SnowSeeker/SnowSeeker/SnowSeeker/ContentView.swift
+++ Project19-SnowSeeker/SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,17 +7,22 @@
 
 import SwiftUI
 
+enum ResortSortOrder {
+    case none, alphabetical, country
+}
+
 struct ContentView: View {
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
-    
+
     @StateObject var favorites = Favorites()
 
     @State private var columnVisibility = NavigationSplitViewVisibility.detailOnly
     @State private var searchText = ""
+    @State private var sortOrder = ResortSortOrder.none
 
     var body: some View {
         NavigationSplitView(columnVisibility: $columnVisibility) {
-            List(filteredResort) { resort in
+            List(sortedResorts) { resort in
                 NavigationLink {
                     ResortView(resort: resort)
                 } label: {
@@ -38,7 +43,7 @@ struct ContentView: View {
                             Text("\(resort.runs) runs")
                                 .foregroundColor(.secondary)
                         }
-                        
+
                         if favorites.contains(resort) {
                             Spacer()
                             Image(systemName: "heart.fill")
@@ -50,6 +55,36 @@ struct ContentView: View {
             }
             .navigationTitle("Resorts")
             .searchable(text: $searchText, prompt: "Search for a resort")
+            .toolbar {
+                Menu {
+                    Button {
+                        sortOrder = .none
+                    } label: {
+                        Text("none")
+                        if sortOrder == .none {
+                            Image(systemName: "checkmark.circle")
+                        }
+                    }
+                    Button {
+                        sortOrder = .alphabetical
+                    } label: {
+                        Text("alphabetical")
+                        if sortOrder == .alphabetical {
+                            Image(systemName: "checkmark.circle")
+                        }
+                    }
+                    Button {
+                        sortOrder = .country
+                    } label: {
+                        Text("country")
+                        if sortOrder == .country {
+                            Image(systemName: "checkmark.circle")
+                        }
+                    }
+                } label: {
+                    Label("Sort List", systemImage: "lineweight")
+                }
+            }
 
         } detail: {
             WelcomeView()
@@ -57,12 +92,25 @@ struct ContentView: View {
         .navigationSplitViewStyle(.automatic)
         .environmentObject(favorites)
     }
-    
-    var filteredResort: [Resort] {
+
+    var filteredResorts: [Resort] {
         if searchText.isEmpty {
             return resorts
         } else {
-            return resorts.filter { $0.name.localizedStandardContains(searchText) }
+            return resorts
+                .filter { $0.name.localizedStandardContains(searchText)}
+        }
+    }
+    var sortedResorts: [Resort] {
+        filteredResorts.sorted { resort1, resort2 in
+            switch sortOrder {
+            case .none:
+                return false
+            case .alphabetical:
+                return resort1.name < resort2.name
+            case .country:
+                return resort1.country < resort2.country
+            }
         }
     }
 }
--- Project19-SnowSeeker/SnowSeeker/SnowSeeker/ResortView.swift
+++ Project19-SnowSeeker/SnowSeeker/SnowSeeker/ResortView.swift
@@ -21,9 +21,21 @@ struct ResortView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
-                Image(decorative: resort.id)
-                    .resizable()
-                    .scaledToFit()
+                ZStack {
+                    Image(decorative: resort.id)
+                        .resizable()
+                        .scaledToFit()
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Image(systemName: "c.circle")
+                            Text("\(resort.imageCredit)")
+                            Spacer()
+                        }
+                        .padding()
+                        .foregroundColor(.secondary)
+                    }
+                }
                 
                 HStack {
                     if horSizeClass == .compact && typeSize > .large {
